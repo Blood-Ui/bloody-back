ed6c392f9b493f8e970cbf15d023eb8ed9cbe8aa
lordgrim <jeromjomanthara@gmail.com>
2024-01-31 14:49:28 +0530

    feat(role): add update view and url

--- api/role/role_urls.py
+++ api/role/role_urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
         path('', RoleDropDownAPIView.as_view(), name='role_list'),
         path('all/', RoleAPIView.as_view(), name='role_detail'), #get
         path('create/', RoleAPIView.as_view(), name='role_create'), #single post
+        path('update/<uuid:role_id>/', RoleAPIView.as_view(), name='role_update'), #single patch
 
     ]
--- api/role/role_view.py
+++ api/role/role_view.py
@@ -38,3 +38,19 @@ class RoleAPIView(APIView):
             serializer.save()
             return Response({"message": "successfully created role", "response": serializer.data}, status=status.HTTP_201_CREATED)
         return Response({"message": "failed to create role", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, role_id):
+        JWT_authenticator = JWTAuthentication()
+        response = JWT_authenticator.authenticate(request)
+        if response is not None:
+            # unpacking
+            user , token = response
+            user_id = token.payload['user_id']
+        role = Role.objects.filter(id=role_id).first()
+        if not role:
+            return Response({"message": "role does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = RoleCreateEditSerializer(role, data=request.data, context={'request': request, 'user_id': user_id})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "successfully updated role", "response": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"message": "failed to update role", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
