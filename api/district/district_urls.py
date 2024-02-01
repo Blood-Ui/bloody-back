fb9d4a8adb3b29c229efa33edf28b6d8eee4b4fe
lordgrim <jeromjomanthara@gmail.com>
2024-02-01 15:10:05 +0530

    feat(district): add update view and url

--- api/district/district_urls.py
+++ api/district/district_urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .district_view import DistrictAPIView, DistrictDropDownAPIView
 
 urlpatterns = [
-    path('', DistrictAPIView.as_view(), name='district_list'), #drop down
+    path('', DistrictDropDownAPIView.as_view(), name='district_list'), #drop down
     path('all/', DistrictAPIView.as_view(), name='district_detail'), #get
     path('create/', DistrictAPIView.as_view(), name='district_create'), #single post
+    path('update/<uuid:district_id>/', DistrictAPIView.as_view(), name='district_update'), #single patch
 
 ]
--- api/district/district_view.py
+++ api/district/district_view.py
@@ -29,6 +29,22 @@ class DistrictAPIView(APIView):
             return Response({"message": "successfully created district", "response": serializer.data}, status=status.HTTP_201_CREATED)
         return Response({"message": "failed to create district", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
     
+    def update(self, request, district_id):
+        JWT_authenticator = JWTAuthentication()
+        response = JWT_authenticator.authenticate(request)
+        if response is not None:
+            # unpacking
+            user , token = response
+            user_id = token.payload['user_id']
+        district = District.objects.get(id=district_id)
+        if not district:
+            return Response({"message": "district not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = DistrictCreateEditSerializer(district, data=request.data, context={'request': request, 'user_id': user_id})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "successfully updated district", "response": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"message": "failed to update district", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
 class DistrictDropDownAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
