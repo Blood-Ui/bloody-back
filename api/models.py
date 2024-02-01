2669362be748dad7c86aef450b4242e683554b58
lordgrim <jeromjomanthara@gmail.com>
2024-02-01 14:41:44 +0530

    feat(api): add district to models

--- api/models.py
+++ api/models.py
@@ -26,3 +26,11 @@ class Blood_Group(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blood_group_created_by')
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blood_group_updated_by')
+
+class District(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='updated_by')
