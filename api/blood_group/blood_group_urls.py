60a96d5f84f1dd83b65ffcae9c22cc4119d2b1a2
lordgrim <jeromjomanthara@gmail.com>
2024-02-01 20:04:36 +0530

    fix(blood_group): urls

--- api/blood_group/blood_group_urls.py
+++ api/blood_group/blood_group_urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from blood_group_view import Blood_Group_DropdownAPIView, Blood_Group_APIview
+from .blood_group_view import Blood_Group_DropdownAPIview, Blood_Group_APIview
 
 urlpatterns = [
-        path('', Blood_Group_DropdownAPIView.as_view(), name='blood_group_list'), #drop down
+        path('', Blood_Group_DropdownAPIview.as_view(), name='blood_group_list'), #drop down
         path('all/', Blood_Group_APIview.as_view(), name='blood_group_detail'), #get
         path('create/', Blood_Group_APIview.as_view(), name='blood_group_create'), #single post
         path('update/<uuid:blood_group_id>/', Blood_Group_APIview.as_view(), name='blood_group_update'), #single patch
