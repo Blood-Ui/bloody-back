1405c908ec3bc11313a6244eb130109dcd49ba64
lordgrim <jeromjomanthara@gmail.com>
2024-02-01 15:23:14 +0530

    feat(city): add CityCreateSerializer and create method

---
+++ api/city/city_serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from api.models import City
+
+class CityCreateSerializer(serializers.ModelSerializer):
+    district = serializers.CharField(source='district.name')
+
+    class Meta:
+        model = City
+        fields = ['name', 'district']
+
+    def create(self, validated_data):
+        user_id = self.context["user_id"]
+
+        validated_data["district_id"] = validated_data.pop("district")
+        validated_data["created_by_id"] = user_id
+        validated_data["updated_by_id"] = user_id
+        city = City.objects.create(**validated_data)
+        return city
