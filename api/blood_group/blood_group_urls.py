6d8bfcd055c18d483fc2defaec2b4ba29d97179c
lordgrim <jeromjomanthara@gmail.com>
2024-02-01 14:27:47 +0530

    feat(blood_group): add create view and url

--- api/blood_group/blood_group_urls.py
+++ api/blood_group/blood_group_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blood_group_view import Blood_Group_DropdownAPIView
+from blood_group_view import Blood_Group_DropdownAPIView, Blood_Group_APIview
 
 urlpatterns = [
         path('', Blood_Group_DropdownAPIView.as_view(), name='blood_group_list'), #drop down
-        path('all/', Blood_Group_DropdownAPIView.as_view(), name='blood_group_detail'), #get
+        path('all/', Blood_Group_APIview.as_view(), name='blood_group_detail'), #get
+        path('create/', Blood_Group_APIview.as_view(), name='blood_group_create'), #single post
 
     ]
--- api/blood_group/blood_group_view.py
+++ api/blood_group/blood_group_view.py
@@ -14,7 +14,7 @@ class Blood_Group_DropdownAPIview(APIView):
     def get(self, request):
         blood_groups = Blood_Group.objects.all()
         serializer = BloodGroupDropDownSerizlizer(blood_groups, many=True)
-        return Response({"message": "successfully obtained roles", "response": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"message": "successfully obtained blood groups", "response": serializer.data}, status=status.HTTP_200_OK)
     
 class Blood_Group_APIview(APIView):
     permission_classes = [IsAuthenticated]
@@ -22,4 +22,17 @@ class Blood_Group_APIview(APIView):
     def get(self, request):
         blood_groups = Blood_Group.objects.all()
         serializer = BloodGroupListSerializer(blood_groups, many=True)
-        return Response({"message": "successfully obtained roles", "response": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"message": "successfully obtained blood groups", "response": serializer.data}, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        JWT_authenticator = JWTAuthentication()
+        response = JWT_authenticator.authenticate(request)
+        if response is not None:
+            # unpacking
+            user , token = response
+            user_id = token.payload['user_id']
+        serializer = BloodGroupListSerializer(data=request.data, context={'request': request, 'user_id': user_id})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "successfully created blood group", "response": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({"message": "failed to blood group", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
