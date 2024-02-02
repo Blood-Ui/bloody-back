ea07fd8a9134d5558e829f914331fd74074e5d17
lordgrim <jeromjomanthara@gmail.com>
2024-02-02 15:56:48 +0530

    feat(donor): add update view and url

--- api/donor/donor_urls.py
+++ api/donor/donor_urls.py
@@ -4,5 +4,6 @@ from.donor_view import DonorAPIView
 urlpatterns = [
     path('all/', DonorAPIView.as_view(), name='donor_detail'), #get
     path('create/', DonorAPIView.as_view(), name='donor_create'), #single post
+    path('update/<uuid:donor_id>/', DonorAPIView.as_view(), name='donor_update'), #single patch
 
 ]
--- api/donor/donor_view.py
+++ api/donor/donor_view.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from api.models import Donor
-from .donor_serializer import DonorCreateSerializer, DonorListSerializer
+from .donor_serializer import DonorCreateUpdateSerializer, DonorListSerializer
 
 
 class DonorAPIView(APIView):
@@ -19,7 +19,7 @@ class DonorAPIView(APIView):
             user , token = response
             user_id = token.payload['user_id']
 
-        serializer = DonorCreateSerializer(data=request.data, context={"request": request, "user_id": user_id})
+        serializer = DonorCreateUpdateSerializer(data=request.data, context={"request": request, "user_id": user_id})
         if serializer.is_valid():
             serializer.save()
             return Response({"message": "successfully created donor", "response": serializer.data}, status=status.HTTP_201_CREATED)
@@ -29,3 +29,20 @@ class DonorAPIView(APIView):
         donors = Donor.objects.all()
         serializer = DonorListSerializer(donors, many=True)
         return Response({"message": "successfully fetched donors", "response": serializer.data}, status=status.HTTP_200_OK)
+    
+    def patch(self, request, donor_id):
+        JWT_authenticator = JWTAuthentication()
+        response = JWT_authenticator.authenticate(request)
+        if response is not None:
+            # unpacking
+            user , token = response
+            user_id = token.payload['user_id']
+
+        donor = Donor.objects.get(id=donor_id)
+        if donor is None:
+            return Response({"message": "donor not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = DonorCreateUpdateSerializer(donor, data=request.data, context={"request": request, "user_id": user_id})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "successfully updated donor", "response": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({"message": "failed to update donor", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
