2a52b949564305789ea6965014e3ec91cf4b1720
lordgrim <jeromjomanthara@gmail.com>
2024-02-02 15:43:05 +0530

    feat(api): add city to urls

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('role/', include('api.role.role_urls')),
     path('blood-group/', include('api.blood_group.blood_group_urls')),
     path('district/', include('api.district.district_urls')),
+    path('city/', include('api.city.city_urls')),
     
 ]
