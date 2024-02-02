002fe0d52244f534041c584204f3a57c4953a36f
lordgrim <jeromjomanthara@gmail.com>
2024-02-02 19:08:55 +0530

    fix(city): use drop down view for default url

--- api/city/city_urls.py
+++ api/city/city_urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .city_view import CityAPIView
+from .city_view import CityAPIView, CityDropDownView
 
 urlpatterns = [
-    path('', CityAPIView.as_view(), name='city_list'), #drop down
+    path('', CityDropDownView.as_view(), name='city_list'), #drop down
     path('all/', CityAPIView.as_view(), name='city_detail'), #get
     path('create/', CityAPIView.as_view(), name='city_create'), #single post
     path('update/<uuid:city_id>/', CityAPIView.as_view(), name='city_update'), #single patch
