0d780022ab129d25077eeca54340ba5d7dae6658
lordgrim <jeromjomanthara@gmail.com>
2024-02-02 16:17:57 +0530

    feat(patient): add update view and url

--- api/patient/patient_urls.py
+++ api/patient/patient_urls.py
@@ -3,4 +3,5 @@ from .patient_view import PatientAPIView
 
 urlpatterns = [
     path('create/', PatientAPIView.as_view(), name='patient_create'), #single post
+    path('update/<uuid:patient_id>/', PatientAPIView.as_view(), name='patient_update'), #single patch
 ]
--- api/patient/patient_view.py
+++ api/patient/patient_view.py
@@ -24,3 +24,21 @@ class PatientAPIView(APIView):
             serializer.save()
             return Response({"message": "successfully created patient", "response": serializer.data}, status=status.HTTP_201_CREATED)
         return Response({"message": "failed to create patient", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, patient_id):
+        JWT_authenticator = JWTAuthentication()
+        response = JWT_authenticator.authenticate(request)
+        if response is not None:
+            # unpacking
+            user , token = response
+            user_id = token.payload['user_id']
+
+        patient = Patient.objects.filter(id=patient_id, created_by_id=user_id).first()
+        if not patient:
+            return Response({"message": "patient does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PatientCreateUpdateSerializer(patient, data=request.data, context={"request": request, "user_id": user_id}, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "successfully updated patient", "response": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"message": "failed to update patient", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
