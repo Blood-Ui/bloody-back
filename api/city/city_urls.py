aa8275e1df901b3f47fad9fb17e581431a02375f
lordgrim <jeromjomanthara@gmail.com>
2024-02-02 22:55:38 +0530

    perf(api): change update and delete url to take id as str instead if uuid

--- api/city/city_urls.py
+++ api/city/city_urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', CityDropDownView.as_view(), name='city_list'), #drop down
     path('all/', CityAPIView.as_view(), name='city_detail'), #get
     path('create/', CityAPIView.as_view(), name='city_create'), #single post
-    path('update/<uuid:city_id>/', CityAPIView.as_view(), name='city_update'), #single patch
-    path('delete/<uuid:city_id>/', CityAPIView.as_view(), name='city_delete'), #single delete
+    path('update/<str:city_id>/', CityAPIView.as_view(), name='city_update'), #single patch
+    path('delete/<str:city_id>/', CityAPIView.as_view(), name='city_delete'), #single delete
     
 ]
--- api/district/district_urls.py
+++ api/district/district_urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', DistrictDropDownAPIView.as_view(), name='district_list'), #drop down
     path('all/', DistrictAPIView.as_view(), name='district_detail'), #get
     path('create/', DistrictAPIView.as_view(), name='district_create'), #single post
-    path('update/<uuid:district_id>/', DistrictAPIView.as_view(), name='district_update'), #single patch
-    path('delete/<uuid:district_id>/', DistrictAPIView.as_view(), name='district_delete'), #single delete
+    path('update/<str:district_id>/', DistrictAPIView.as_view(), name='district_update'), #single patch
+    path('delete/<str:district_id>/', DistrictAPIView.as_view(), name='district_delete'), #single delete
 
 ]
