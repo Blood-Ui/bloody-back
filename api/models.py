db2c8855336dfd0b8cfd6d74eaccafc85626681b
lordgrim <jeromjomanthara@gmail.com>
2024-02-02 22:53:27 +0530

    perf(api): change id field of models to charfield

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from auth_setup.models import User
 import uuid
 
 class Role(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30, unique=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -11,7 +11,7 @@ class Role(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='role_updated_by')
 
 class UserRoleLink(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -20,7 +20,7 @@ class UserRoleLink(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_role_updated_by')
 
 class Blood_Group(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(max_length=36, primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30, unique=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -28,7 +28,7 @@ class Blood_Group(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blood_group_updated_by')
 
 class District(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30, unique=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -36,7 +36,7 @@ class District(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='district_updated_by')
 
 class City(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30, unique=True, blank=False)
     district = models.ForeignKey(District, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -45,7 +45,7 @@ class City(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='city_updated_by')
 
 class Organization(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30, unique=True, blank=False)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -54,7 +54,7 @@ class Organization(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organization_updated_by')
 
 class UserOrganizationLink(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -63,7 +63,7 @@ class UserOrganizationLink(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_organization_updated_by')
 
 class Donor(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, blank=False)
     phone_number = models.CharField(max_length=15, unique=True, blank=False)
     email = models.EmailField(max_length=255, unique=True, blank=False)
@@ -76,7 +76,7 @@ class Donor(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='donor_updated_by')
 
 class Patient(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True, blank=False)
     bystander_name = models.CharField(max_length=255, blank=False)
     bystander_phone_number = models.CharField(max_length=15, blank=False)
@@ -89,7 +89,7 @@ class Patient(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patient_updated_by')
 
 class Request(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     status = models.CharField(max_length=30, blank=False, default='Pending')
     created_at = models.DateTimeField(auto_now_add=True)
