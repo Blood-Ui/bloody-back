fe7428d402eae1150af8af73065a7042c1505e40
lordgrim <jeromjomanthara@gmail.com>
2024-02-04 12:40:20 +0530

    fix(donor): create new DonorUpdateSerializer with update method and utilize it in views

--- api/donor/donor_serializer.py
+++ api/donor/donor_serializer.py
@@ -29,6 +29,50 @@ class DonorCreateSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("City does not exist")
         return value
     
+class DonorUpdateSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+    phone_number = serializers.CharField(required=False)
+    date_of_birth = serializers.DateField(required=False)
+    blood_group = serializers.CharField(required=False)
+    city = serializers.CharField(required=False)
+
+    class Meta:
+        model = Donor
+        fields = ['name', 'email', 'phone_number', 'date_of_birth', 'blood_group', 'city']
+
+    def update(self, instance, validated_data):
+        user_id = self.context.get("user_id")
+
+        new_name = validated_data.get("name", instance.name)
+        new_email = validated_data.get("email", instance.email)
+        new_phone_number = validated_data.get("phone_number", instance.phone_number)
+        new_date_of_birth = validated_data.get("date_of_birth", instance.date_of_birth)
+        new_blood_group = validated_data.get("blood_group", instance.blood_group_id)
+        new_city = validated_data.get("city", instance.city_id)
+        if Donor.objects.filter(name=new_name, email=new_email, phone_number=new_phone_number, date_of_birth=new_date_of_birth, blood_group_id=new_blood_group, city_id=new_city).exists():
+            raise serializers.ValidationError("Donor already exists")
+
+        instance.name = new_name
+        instance.email = new_email
+        instance.phone_number = new_phone_number
+        instance.date_of_birth = new_date_of_birth
+        instance.blood_group_id = new_blood_group
+        instance.city_id = new_city
+        instance.updated_by_id = user_id
+        instance.save()
+        return instance
+    
+    def validate_blood_group(self, value):
+        if not Blood_Group.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Blood Group does not exist")
+        return value
+    
+    def validate_city(self, value):
+        if not City.objects.filter(id=value).exists():
+            raise serializers.ValidationError("City does not exist")
+        return value
+
 class DonorListSerializer(serializers.ModelSerializer):
     blood_group = serializers.CharField(source='blood_group.name')
     city = serializers.CharField(source='city.name')
--- api/donor/donor_view.py
+++ api/donor/donor_view.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from api.models import Donor
-from .donor_serializer import DonorCreateSerializer, DonorListSerializer, DonorDropDownSerializer
+from .donor_serializer import DonorCreateSerializer, DonorUpdateSerializer, DonorListSerializer, DonorDropDownSerializer
 
 
 class DonorAPIView(APIView):
@@ -30,22 +30,22 @@ class DonorAPIView(APIView):
         serializer = DonorListSerializer(donors, many=True)
         return Response({"message": "successfully fetched donors", "response": serializer.data}, status=status.HTTP_200_OK)
     
-    # def patch(self, request, donor_id):
-    #     JWT_authenticator = JWTAuthentication()
-    #     response = JWT_authenticator.authenticate(request)
-    #     if response is not None:
-    #         # unpacking
-    #         user , token = response
-    #         user_id = token.payload['user_id']
+    def patch(self, request, donor_id):
+        JWT_authenticator = JWTAuthentication()
+        response = JWT_authenticator.authenticate(request)
+        if response is not None:
+            # unpacking
+            user , token = response
+            user_id = token.payload['user_id']
 
-    #     if not Donor.objects.filter(id=donor_id).exists():
-    #         return Response({"message": "donor not found"}, status=status.HTTP_404_NOT_FOUND)
-    #     donor = Donor.objects.get(id=donor_id)
-    #     serializer = DonorCreateUpdateSerializer(donor, data=request.data, context={"request": request, "user_id": user_id})
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response({"message": "successfully updated donor", "response": serializer.data}, status=status.HTTP_201_CREATED)
-    #     return Response({"message": "failed to update donor", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        if not Donor.objects.filter(id=donor_id).exists():
+            return Response({"message": "donor not found"}, status=status.HTTP_404_NOT_FOUND)
+        donor = Donor.objects.get(id=donor_id)
+        serializer = DonorUpdateSerializer(donor, data=request.data, context={"request": request, "user_id": user_id})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "successfully updated donor", "response": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({"message": "failed to update donor", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, donor_id):
         if not Donor.objects.filter(id=donor_id).exists():
