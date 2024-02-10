7d3212bdff8a36c378a9ea149c508f7f60217d0f
lordgrim <jeromjomanthara@gmail.com>
2024-02-10 18:06:09 +0530

    feat(utils): add custom response class

--- api/utils.py
+++ api/utils.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from enum import Enum
 
 def allowed_roles(allowed_roles):
@@ -20,3 +21,13 @@ class RequestStatus(Enum):
     @classmethod
     def get_all_values(cls):
         return [member.value for member in cls]
+    
+
+class CustomResponse(Response):
+    def __init__(self, error=None, message=None, data=None, status_code=None, *args, **kwargs):
+        self.error = '' if error is None else error
+        self.message = {} if message is None else message
+        self.data = {} if data is None else data
+        self.status_code = status.HTTP_200_OK if status_code is 1 else status.HTTP_400_BAD_REQUEST
+
+        return super().__init__(data = {'error': self.error, 'message': self.message, 'data': self.data, 'status_code': self.status_code}, status=self.status_code, *args, **kwargs)
