c0afd8d7029e8c78bacc32ca9d81e588e8611244
lordgrim <jeromjomanthara@gmail.com>
2024-02-15 07:35:42 +0530

    refactor(api): remove useless imports

--- api/patient/patient_view.py
+++ api/patient/patient_view.py
@@ -1,8 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.db import transaction
 
 from api.models import Patient, Request
