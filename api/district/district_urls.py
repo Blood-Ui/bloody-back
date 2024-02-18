d55241f11b160d781a14c848c3e40b277099050e
lordgrim <jeromjomanthara@gmail.com>
2024-02-19 00:03:16 +0530

    feat(district): add bulk import view and url

--- api/district/district_urls.py
+++ api/district/district_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .district_view import DistrictAPIView, DistrictDropDownAPIView
+from .district_view import DistrictAPIView, DistrictDropDownAPIView, DistrictBaseTemplateAPIView, DistrictBulkImportAPIView
 
 urlpatterns = [
     path('', DistrictDropDownAPIView.as_view(), name='district_list'), #drop down
@@ -7,5 +7,8 @@ urlpatterns = [
     path('create/', DistrictAPIView.as_view(), name='district_create'), #single post
     path('update/<str:district_id>/', DistrictAPIView.as_view(), name='district_update'), #single patch
     path('delete/<str:district_id>/', DistrictAPIView.as_view(), name='district_delete'), #single delete
+
+    path('base-template/', DistrictBaseTemplateAPIView.as_view(), name='district_base_template'), #get
+    path('bulk-import/', DistrictBulkImportAPIView.as_view(), name='district_bulk_import'), #bulk post
 
 ]
